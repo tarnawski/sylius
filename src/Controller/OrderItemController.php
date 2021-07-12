3322b9cff86264e5ce67405b689a8fdcdeffc959
Tomasz Tarnawski <ttarnawski@pgs-soft.com>
2021-07-12 21:50:09 +0200

    Add direct link to product variant

---
+++ src/Controller/OrderItemController.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use FOS\RestBundle\View\View;
+use Sylius\Bundle\OrderBundle\Controller\AddToCartCommandInterface;
+use Sylius\Bundle\OrderBundle\Controller\OrderItemController as BaseOrderItemController;
+use Sylius\Component\Order\CartActions;
+use Sylius\Component\Order\Model\OrderItemInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class OrderItemController extends BaseOrderItemController
+{
+    public function addAction(Request $request): Response
+    {
+        $cart = $this->getCurrentCart();
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, CartActions::ADD);
+        /** @var OrderItemInterface $orderItem */
+        $orderItem = $this->newResourceFactory->create($configuration, $this->factory);
+
+        $this->getQuantityModifier()->modify($orderItem, 1);
+
+        $form = $this->getFormFactory()->create(
+            $configuration->getFormType(),
+            $this->createAddToCartCommand($cart, $orderItem),
+            $configuration->getFormOptions()
+        );
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            /** @var AddToCartCommandInterface $addToCartCommand */
+            $addToCartCommand = $form->getData();
+
+            $errors = $this->getCartItemErrors($addToCartCommand->getCartItem());
+            if (0 < count($errors)) {
+                $form = $this->getAddToCartFormWithErrors($errors, $form);
+
+                return $this->handleBadAjaxRequestView($configuration, $form);
+            }
+
+            $event = $this->eventDispatcher->dispatchPreEvent(CartActions::ADD, $configuration, $orderItem);
+
+            if ($event->isStopped() && !$configuration->isHtmlRequest()) {
+                throw new HttpException($event->getErrorCode(), $event->getMessage());
+            }
+            if ($event->isStopped()) {
+                $this->flashHelper->addFlashFromEvent($configuration, $event);
+
+                return $this->redirectHandler->redirectToIndex($configuration, $orderItem);
+            }
+
+            $this->getOrderModifier()->addToOrder($addToCartCommand->getCart(), $addToCartCommand->getCartItem());
+
+            $cartManager = $this->getCartManager();
+            $cartManager->persist($cart);
+            $cartManager->flush();
+
+            $resourceControllerEvent = $this->eventDispatcher->dispatchPostEvent(CartActions::ADD, $configuration, $orderItem);
+            if ($resourceControllerEvent->hasResponse()) {
+                return $resourceControllerEvent->getResponse();
+            }
+
+            $this->flashHelper->addSuccessFlash($configuration, CartActions::ADD, $orderItem);
+
+            if ($request->isXmlHttpRequest()) {
+                return $this->viewHandler->handle($configuration, View::create([], Response::HTTP_CREATED));
+            }
+
+            return $this->redirectHandler->redirectToResource($configuration, $orderItem);
+        }
+
+        if (!$configuration->isHtmlRequest()) {
+            return $this->handleBadAjaxRequestView($configuration, $form);
+        }
+
+        return $this->render(
+            $configuration->getTemplate(CartActions::ADD . '.html'),
+            [
+                'configuration' => $configuration,
+                'options' => $request->query->get('options', []),
+                $this->metadata->getName() => $orderItem,
+                'form' => $form->createView()
+            ]
+        );
+    }
+}
